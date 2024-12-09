1590d6519adbe917b9107927b080bb9377208045
imi56 <iahmad@groupon.com>
2024-12-09 10:28:54 +0530

    Added tic tac toe

--- src/main/java/com/example/explore/App.java
+++ src/main/java/com/example/explore/App.java
@@ -3,7 +3,6 @@ package com.example.explore;
 public class App {
 
     public static void main(String[] args) {
-        App player = new App();
 
     }
 }
--- src/main/java/com/example/explore/classes/Test.java
+++
@@ -1,7 +0,0 @@
-package com.example.explore.classes;
-
-public class Test {
-  public void test() {
-    System.out.println("Printing from Test#test");
-  }
-}
